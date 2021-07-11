61430026ecb8535d37d37ce1d6904577cbe7059b
KeRas1n <80959420+KeRas1n@users.noreply.github.com>
2021-07-11 23:48:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,20 @@ document.body.className = localStorage.theme
 
 toggleThemeBtn.onclick = () => {
 	document.body.classList.toggle("white")
-	document.head.classList.toggle("whitesearch")
 	toggleThemeBtn.innerText = document.body.classList.contains("white") ?"СМЕНИТЬ ТЕМУ НА ТЁМНУЮ": "СМЕНИТЬ ТЕМУ НА СВЕТЛУЮ"
 	localStorage.theme = document.body.className || "black"
 }
+
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Текст скопирован: " + copyText.value);
+} 
