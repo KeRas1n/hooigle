3b7dfbf7e38653f9ced8dca2f23f0864bee04581
KeRas1n <80959420+KeRas1n@users.noreply.github.com>
2021-07-12 17:27:00 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+var IsLen = localStorage.length;
+	if(IsLen > 1){
+		for (var i = 0; i < IsLen; i++) {
+			var key = localStorage.key(i);
+			var href = key;
+			var string = localStorage.getItem(i);/*null*/
+			var str = string;
+			var app = document.querySelector('.app')
+			var title = document.createElement("a"); 
+			
+			
+			if (localStorage.getItem(key).indexOf('www') > -1)
+			{
+		  		title.innerText = href
+				title.setAttribute('href', localStorage.getItem(key));
+				title.className = 'title'
+				app.appendChild(title)
+		  		
+			}
+			
+			
+		}
+	}
+
+var nId = 0;
 if (!localStorage.theme) localStorage.theme = "black"
 document.body.className = localStorage.theme
 
@@ -20,3 +45,57 @@ function myFunction() {
   /* Alert the copied text */
   alert("Текст скопирован: " + copyText.value);
 } 
+
+function addobj(){
+	nId  = nId+1;
+	var href = document.getElementById("urltitle").value;
+	var string = document.getElementById("urlinput").value;
+	var app = document.querySelector('.app')
+	var title = document.createElement("a");   
+	
+	
+	title.setAttribute('href', string);
+	title.className = 'title'
+	
+	if (string === "") {
+    alert("Заполните URL и NAME");
+    return;
+	}
+	if (href === "") {
+    alert("Заполните URL и NAME");
+    return;
+	}
+
+
+	title.innerText = href
+
+
+	app.appendChild(title)
+	
+   	localStorage.setItem(href, title);
+	/*urlinput.select();
+
+  /* Copy the text inside the text field */
+ /* document.execCommand("copy");*/
+}
+function showTasks(){
+	var IsLen = localStorage.length;
+	if(IsLen > 1){
+		for (var i = 0; i < IsLen; i++) {
+			var key = localStorage.key(i);
+			var href = key;
+			var string = localStorage.getItem(tits);
+			var str = string;
+			var app = document.querySelector('.app')
+			var title = document.createElement("a"); 
+			
+			title.innerText = href
+			title.setAttribute('href', str);
+			title.className = 'title'
+			app.appendChild(title)
+		}
+	}
+}
+function wipedata(){
+	localStorage.clear(); 
+}
