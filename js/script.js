e9a9bd305dda66518f43336e03b81bfad534633f
KeRas1n <80959420+KeRas1n@users.noreply.github.com>
2021-09-04 19:54:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
 //RadioTHEME
 if (!localStorage.radioTheme) localStorage.radioTheme = "1"
 var option = document.getElementsByName("r");
